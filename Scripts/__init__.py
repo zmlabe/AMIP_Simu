ee34275aadb621639df274a946a5eb658ba56f28
Zachary Labe <zlabe@uci.edu>
2019-02-04 15:53:53 -0800

    First commit of project

---
+++ Scripts/__init__.py
@@ -0,0 +1,9 @@
+'''
+The Python repository "AMIP" contains the scripts, data, 
+figures, and text for 6 AMIP experiments (CSIC,CSST,AMS,
+AMIP,AMQ,AMQS) conducted using SC-WACCM4. We are interested
+in the relative roles of surface forcings, including sea ice,
+Siberian snow cover, sea surface temperatures, and the QBO 
+on the large-scale atmospheric response over the satellite era.
+Author: Zachary Labe (zlabe@uci.edu)
+'''
