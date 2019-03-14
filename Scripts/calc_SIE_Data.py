3cf614ce8c03c7dcbc0837f33c2c92225b8d8833
Zachary Labe <zlabe@uci.edu>
2019-03-14 14:54:57 -0700

    low SIE composites

--- Scripts/calc_SIE_Data.py
+++ Scripts/calc_SIE_Data.py
@@ -31,7 +31,7 @@ titletime = currentmn + '/' + currentdy + '/' + currentyr
 print('\n' '----Calculating Sea Ice Extent Data - %s----' % titletime)
 
 #### Alott time series
-year1 = 1978
+year1 = 1979
 year2 = 2016
 years = np.arange(year1,year2+1,1)
 
@@ -49,7 +49,7 @@ def readSIEData():
     data = Dataset(directorydata + filename)
     lat = data.variables['latitude'][:]
     lon = data.variables['longitude'][:]
-    sic = data.variables['SIC'][:]
+    sic = data.variables['SIC'][12:]
     data.close()
     
     ### Mask data 
@@ -113,8 +113,32 @@ extstd = np.std(ext,axis=0)
 ### Calculate OND sea ice index
 extond = np.nanmean(ext[:,-3:],axis=1)
 extondzz = sts.zscore(extond,axis=0)
-iceslice_ond = np.where(extondzz <= -1.)[0]
-yearslice_ond = years[iceslice_ond]
+iceslice05_ond = np.where(extondzz <= -0.5)[0]
+yearslice05_ond = years[iceslice05_ond]
+iceslice1_ond = np.where(extondzz <= -1.)[0]
+yearslice1_ond = years[iceslice1_ond]
+iceslice2_ond = np.where(extondzz <= -2.)[0]
+yearslice2_ond = years[iceslice2_ond]
+
+### Calculate D sea ice index
+extd = np.nanmean(ext[:,-1:],axis=1)
+extdzz = sts.zscore(extd,axis=0)
+iceslice05_d = np.where(extdzz <= -0.5)[0]
+yearslice05_d = years[iceslice05_d]
+iceslice1_d = np.where(extdzz <= -1.)[0]
+yearslice1_d = years[iceslice1_d]
+iceslice2_d = np.where(extdzz <= -2.)[0]
+yearslice2_d = years[iceslice2_d]
+
+### Calculate JFM sea ice index
+extjfm = np.nanmean(ext[:,0:3],axis=1)
+extjfmzz = sts.zscore(extjfm,axis=0)
+iceslice05_jfm = np.where(extjfmzz <= -0.5)[0]
+yearslice05_jfm = years[iceslice05_jfm]
+iceslice1_jfm = np.where(extjfmzz <= -1.)[0]
+yearslice1_jfm = years[iceslice1_jfm]
+iceslice2_jfm = np.where(extjfmzz <= -2.)[0]
+yearslice2_jfm = years[iceslice2_jfm]
 
 ### Save data files
 np.savetxt(directoryoutput + 'Monthly_SeaIceExtent.txt',
@@ -125,18 +149,48 @@ np.savetxt(directoryoutput + 'Monthly_SeaIceExtent_ZScore_Detrended.txt',
            extdtzz.ravel())
 np.savetxt(directoryoutput + 'Monthly_SeaIceExtent_STD.txt',
            extstd.ravel())
+
+### Save OND file
 np.savetxt(directoryoutput + 'OND_SeaIceExtent.txt',
            extond)
 np.savetxt(directoryoutput + 'OND_SeaIceExtent_ZScore.txt',
            extondzz)
+np.savetxt(directoryoutput + 'OND_SeaIceExtent_05SigmaYears.txt',
+           np.c_[yearslice05_ond,iceslice05_ond])
 np.savetxt(directoryoutput + 'OND_SeaIceExtent_1SigmaYears.txt',
-           np.c_[yearslice_ond,iceslice_ond])
+           np.c_[yearslice1_ond,iceslice1_ond])
+np.savetxt(directoryoutput + 'OND_SeaIceExtent_2SigmaYears.txt',
+           np.c_[yearslice2_ond,iceslice2_ond])
+
+### Save D file
+np.savetxt(directoryoutput + 'D_SeaIceExtent.txt',
+           extd)
+np.savetxt(directoryoutput + 'D_SeaIceExtent_ZScore.txt',
+           extdzz)
+np.savetxt(directoryoutput + 'D_SeaIceExtent_05SigmaYears.txt',
+           np.c_[yearslice05_d,iceslice05_d])
+np.savetxt(directoryoutput + 'D_SeaIceExtent_1SigmaYears.txt',
+           np.c_[yearslice1_d,iceslice1_d])
+np.savetxt(directoryoutput + 'D_SeaIceExtent_2SigmaYears.txt',
+           np.c_[yearslice2_d,iceslice2_d])
+
+### Save JFM file
+np.savetxt(directoryoutput + 'JFM_SeaIceExtent.txt',
+           extjfm)
+np.savetxt(directoryoutput + 'JFM_SeaIceExtent_ZScore.txt',
+           extjfmzz)
+np.savetxt(directoryoutput + 'JFM_SeaIceExtent_05SigmaYears.txt',
+           np.c_[yearslice05_jfm,iceslice05_jfm])
+np.savetxt(directoryoutput + 'JFM_SeaIceExtent_1SigmaYears.txt',
+           np.c_[yearslice1_jfm,iceslice1_jfm])
+np.savetxt(directoryoutput + 'JFM_SeaIceExtent_2SigmaYears.txt',
+           np.c_[yearslice2_jfm,iceslice2_jfm])
 
 ### Create temporary figures
 plt.rc('text',usetex=True)
 plt.rc('font',**{'family':'sans-serif','sans-serif':['Avant Garde']}) 
 
-sic1 = np.reshape(sic,(468//12,12,lat.shape[0],lon.shape[0]))
+sic1 = np.reshape(sic,(456//12,12,lat.shape[0],lon.shape[0]))
 fig = plt.figure()
 m = Basemap(projection='npstere',boundinglat=30,lon_0=0,
                                 resolution='l',round =True,area_thresh=10000.)
@@ -172,3 +226,30 @@ plt.plot(years,np.nanmean(ext[:,-3:],axis=1),marker='o',markersize=7,
          linewidth=5,color='forestgreen',clip_on=False)
 plt.xlim([1978,2016])
 plt.savefig(directoryfigure + 'ONDSIE.png',dpi=300)
+
+fig = plt.figure()
+plt.plot(years,extondzz,marker='o',markersize=7,
+         linewidth=5,color='forestgreen',clip_on=False)
+plt.axhline(-0.5,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.axhline(-1,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.axhline(-2,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.xlim([1979,2016])
+plt.savefig(directoryfigure + 'ONDSIE_zscore.png',dpi=300)
+
+fig = plt.figure()
+plt.plot(years,extdzz,marker='o',markersize=7,
+         linewidth=5,color='forestgreen',clip_on=False)
+plt.axhline(-0.5,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.axhline(-1,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.axhline(-2,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.xlim([1979,2016])
+plt.savefig(directoryfigure + 'DSIE_zscore.png',dpi=300)
+
+fig = plt.figure()
+plt.plot(years,extjfmzz,marker='o',markersize=7,
+         linewidth=5,color='forestgreen',clip_on=False)
+plt.axhline(-0.5,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.axhline(-1,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.axhline(-2,color='dimgrey',linestyle='--',dashes=(1,0.3),linewidth=2)
+plt.xlim([1979,2016])
+plt.savefig(directoryfigure + 'DSIE_zscore.png',dpi=300)
---
+++ Scripts/plot_LowSeaIceYears.py
@@ -0,0 +1,197 @@
+"""
+Script calculates anomaly composites of low sea ice years
+
+Notes
+-----
+    Author : Zachary Labe
+    Date   : 13 March 2019
+"""
+
+### Import modules
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import cmocean
+from mpl_toolkits.basemap import Basemap, addcyclic, shiftgrid
+import read_MonthlyData as MOM
+import read_Reanalysis as MOR
+import calc_Utilities as UT
+
+### Define directories
+directoryfigure = '/home/zlabe/Desktop/LowSeaIceYears_D/'
+directorydata = '/home/zlabe/Documents/Research/AMIP/Data/'
+
+### Define time           
+now = datetime.datetime.now()
+currentmn = str(now.month)
+currentdy = str(now.day)
+currentyr = str(now.year)
+currenttime = currentmn + '_' + currentdy + '_' + currentyr
+titletime = currentmn + '/' + currentdy + '/' + currentyr
+print('\n' '----Plotting Low Sea Ice Year Composites - %s----' % titletime)
+
+#### Alott time series
+year1 = 1979
+year2 = 2016
+years = np.arange(year1,year2+1,1)
+
+### Add parameters
+ensembles = 10
+su = [0,1,2,3,5,6,7]
+period = 'D'
+varnames = ['SLP','Z500','U200','U10','T2M','THICK']
+runnames = [r'ERA-I',r'CSST',r'CSIC',r'AMIP',r'AMQ',r'AMS',r'AMQS']
+runnamesm = [r'CSST',r'CSIC',r'AMIP',r'AMQ',r'AMS',r'AMQS']
+
+for rr in range(len(varnames)):
+    def readVar(varnames,runnamesm,period):
+        ### Call function to read in ERA-Interim (detrended)
+        lat,lon,time,lev,era = MOR.readDataR(varnames,'surface',True,True)
+        
+        ### Call functions to read in WACCM data (detrended)
+        models = np.empty((len(runnamesm),ensembles,era.shape[0],era.shape[1],
+                           era.shape[2],era.shape[3]))
+        for i in range(len(runnamesm)):
+            lat,lon,time,lev,models[i] = MOM.readDataM(varnames,runnamesm[i],
+                                                       'surface',True,True)
+        
+        return models,era,lat,lon
+    
+    ### Read in data from simulations and ERA-Interim
+    mod,era,lat,lon = readVar(varnames[rr],runnamesm,period)
+    
+    ### Read in low sea ice year slices
+    yearslice,iceslice = np.genfromtxt(directorydata + 'OND_SeaIceExtent_1SigmaYears.txt',
+                                       unpack=True)
+    yearslice = yearslice.astype(int)
+    iceslice = iceslice.astype(int)
+    
+    ### Calculate anomalies
+    eramean = np.nanmean(era,axis=0)
+    modmean = np.nanmean(mod,axis=2)
+    
+    eraanomq = era - eramean
+    modanomq = np.empty((mod.shape))
+    for i in range(mod.shape[0]):
+        for j in range(mod.shape[1]):
+            modanomq[i,j,:,:,:] = mod[i,j,:,:,:,:] - modmean[i,j,:,:,:]
+            
+    if period == 'ND':
+        eraanom = np.nanmean(eraanomq[:,-2:,:,:],axis=1)
+        modanom = np.nanmean(modanomq[:,:,:,-2:,:,:],axis=3)
+    elif period == 'D':
+        eraanom = np.nanmean(eraanomq[:,-1:,:,:],axis=1)
+        modanom = np.nanmean(modanomq[:,:,:,-1:,:,:],axis=3)
+    elif period == 'DJF':  
+        modanom = np.empty((len(runnamesm),ensembles,modanomq.shape[2]-1,
+                         modanomq.shape[4],modanomq.shape[5]))
+        for i in range(len(runnamesm)):
+            for j in range(ensembles):
+                modanom[i,j,:,:,:] = UT.calcDecJanFeb(modanomq[i,j,:,:,:],
+                                                    lat,lon,'surface',1)
+        eraanom = UT.calcDecJanFeb(eraanomq,lat,lon,'surface',1)
+        
+        iceslice = iceslice[:-1]
+            
+    ### Slice low sea ice years
+    eralowm = np.nanmean(eraanom[iceslice,:,:],axis=0)
+    modlowm = np.nanmean(modanom[:,:,iceslice,:,:],axis=2)
+    
+    ### Calculate ensemble mean
+    modmeanlowm = np.nanmean(modlowm,axis=1)
+        
+    ###########################################################################
+    ###########################################################################
+    ###########################################################################
+    ### Plot low sea ice year composites
+    plt.rc('text',usetex=True)
+    plt.rc('font',**{'family':'sans-serif','sans-serif':['Avant Garde']}) 
+    
+    ### Set limits for contours and colorbars
+    if varnames[rr] == 'T2M':
+        limit = np.arange(-2,2.01,0.05)
+        barlim = np.arange(-2,3,1)
+        cmap = cmocean.cm.balance
+        label = r'\textbf{$^{\circ}$C}'
+    elif varnames[rr] == 'Z500':
+        limit = np.arange(-20,20.1,1)
+        barlim = np.arange(-20,21,10)
+        cmap = cmocean.cm.balance
+        label = r'\textbf{m}'
+    elif varnames[rr] == 'Z50':
+        limit = np.arange(-50,50.1,1)
+        barlim = np.arange(-50,51,25)
+        cmap = cmocean.cm.balance
+        label = r'\textbf{m}'
+    elif varnames[rr] == 'U200':
+        limit = np.arange(-5,5.1,0.25)
+        barlim = np.arange(-5,6,5)
+        cmap = cmocean.cm.balance
+        label = r'\textbf{m/s}'
+    elif varnames[rr] == 'U10':
+        limit = np.arange(-5,5.1,0.25)
+        barlim = np.arange(-5,6,5)
+        cmap = cmocean.cm.balance
+        label = r'\textbf{m/s}'
+    elif varnames[rr] == 'SLP':
+        limit = np.arange(-3,3.1,0.25)
+        barlim = np.arange(-3,4,3)
+        cmap = cmocean.cm.balance
+        label = r'\textbf{hPa}'
+    elif varnames[rr] == 'THICK':
+        limit = np.arange(-20,20.1,1)
+        barlim = np.arange(-20,21,5)
+        cmap = cmocean.cm.balance
+        label = r'\textbf{m}'
+    fig = plt.figure(figsize=(6,5))
+    for i in range(len(runnames)):
+        if i == 0:
+            var = eralowm
+    #        pvar = pera
+        else:
+            var = modmeanlowm[i-1]
+    #        pvar = pmodel[i-1]
+        
+        ax1 = plt.subplot(3,4,su[i]+1)
+        m = Basemap(projection='ortho',lon_0=0,lat_0=89,resolution='l',
+                    area_thresh=10000.)
+        
+        var, lons_cyclic = addcyclic(var, lon)
+        var, lons_cyclic = shiftgrid(180., var, lons_cyclic, start=False)
+    #    pvar,lons_cyclic = addcyclic(pvar, lon)
+    #    pvar,lons_cyclic = shiftgrid(180.,pvar,lons_cyclic,start=False)
+        lon2d, lat2d = np.meshgrid(lons_cyclic, lat)
+        x, y = m(lon2d, lat2d)
+           
+        circle = m.drawmapboundary(fill_color='white',color='dimgray',
+                          linewidth=0.7)
+        circle.set_clip_on(False)
+        
+        cs = m.contourf(x,y,var,limit,extend='both')
+    #    cs1 = m.contourf(x,y,pvar,colors='None',hatches=['....'],
+    #                     linewidths=0.4)
+                  
+        m.drawcoastlines(color='dimgray',linewidth=0.7)
+                
+        cs.set_cmap(cmap) 
+        ax1.annotate(r'\textbf{%s}' % runnames[i],xy=(0,0),xytext=(0.865,0.91),
+                     textcoords='axes fraction',color='k',fontsize=11,
+                     rotation=320,ha='center',va='center')
+    
+    ###########################################################################
+    cbar_ax = fig.add_axes([0.412,0.23,0.4,0.03])                
+    cbar = fig.colorbar(cs,cax=cbar_ax,orientation='horizontal',
+                        extend='max',extendfrac=0.07,drawedges=False)
+    
+    cbar.set_label(label,fontsize=11,color='dimgrey',labelpad=1.4)  
+    
+    cbar.set_ticks(barlim)
+    cbar.set_ticklabels(list(map(str,barlim)))
+    cbar.ax.tick_params(axis='x', size=.01,labelsize=8)
+    cbar.outline.set_edgecolor('dimgrey')
+    
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.85,wspace=0,hspace=0.01)
+    
+    plt.savefig(directoryfigure + '%s/LowSeaIceYears_1_%s_%s.png' % (period,varnames[rr],
+                period),dpi=300)
